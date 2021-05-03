c2b235fcf52215e0d082d7d9c08febaf985acc13
Shlomo Bluth <lomibluth@gmail.com>
2021-05-03 10:04:36 +0300

    add commit

--- cypress/integration/RequestsTests.js
+++ cypress/integration/RequestsTests.js
@@ -4,30 +4,52 @@
 
 ////run tests on requests from remove nikud run 
 
-describe('RequestsTests',()=>{
-    
-    beforeEach(() => {
-        cy.visit('https://removenikud.dicta.org.il/')
-    })
-    
-  
+let sizes = ['iphone-x',[1000, 660]]
 
-    it('Error message for tipsoundplay response with status code 500 when clicking the run button of wordplay page'
-    ,()=>{
-       cy.removeNikudRequest({
-           url:'/remove-nikud/api',
-           status:500,
-           message: 'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-       })
-    })
 
+
+sizes.forEach((size) => {
+
+
+    describe('RequestsTests',()=>{
     
-    it('Error message for tipsoundplay response with a delay of 3 minutes when clicking the run button'+
-    ' of wordplay page',()=>{
-        cy.removeNikudRequest({
-            url:'/remove-nikud/api',
-            message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-            delaySeconds: 60*3
+        beforeEach(() => {
+            if (Cypress._.isArray(size)) {
+                Cypress.config({
+                    viewportWidth: size[0],
+                    viewportHeight: size[1]
+                })
+                cy.viewport(size[0], size[1])
+            } else {
+                Cypress.config({
+                    viewportWidth: 375,
+                    viewportHeight: 812
+                })
+                cy.viewport(size)
+            }
+            cy.visit('/')
         })
+        
+      
+    
+        it('Error message for tipsoundplay response with status code 500 when clicking the run button of wordplay page'
+        ,()=>{
+           cy.removeNikudRequest({
+               url:'/api',
+               status:500,
+               message: 'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+           })
+        })
+    
+        
+        // it('Error message for tipsoundplay response with a delay of 3 minutes when clicking the run button'+
+        // ' of wordplay page',()=>{
+        //     cy.removeNikudRequest({
+        //         url:'/api',
+        //         message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+        //         delaySeconds: 60*3
+        //     })
+        // })
     })
 })
+
--- cypress/integration/basicTests.js
+++ cypress/integration/basicTests.js
@@ -2,20 +2,42 @@
 
 //run basic tests on remove nikud.
 
-describe('basicTests',()=>{
+let sizes = ['iphone-x',[1000, 660]]
 
-    beforeEach(() => {
-        cy.visit('https://removenikud.dicta.org.il/')
-    })
 
-    
-    it('Remove nikud.',()=>{
-        cy.get('#nikud-text').type('מֹשֶׁה קִ⁠בֵּל תּוֹרָה מִסִּינַי')
-        cy.get('[class="spinner"]').should('not.exist')
-        cy.get('.results-content ').each($res=>{
-            cy.get($res).should('contain','משה קי⁠בל תורה מסיני')
+
+sizes.forEach((size) => {
+
+    describe('basicTests',()=>{
+
+        beforeEach(() => {
+            if (Cypress._.isArray(size)) {
+                Cypress.config({
+                    viewportWidth: size[0],
+                    viewportHeight: size[1]
+                })
+                cy.viewport(size[0], size[1])
+            } else {
+                Cypress.config({
+                    viewportWidth: 375,
+                    viewportHeight: 812
+                })
+                cy.viewport(size)
+            }
+            cy.visit('/')
         })
+    
+        
+        it('Remove nikud.',()=>{
+            cy.get('#nikud-text').type('מֹשֶׁה קִ⁠בֵּל תּוֹרָה מִסִּינַי')
+            cy.get('[class="spinner"]').should('not.exist')
+            cy.get('.results-content ').each($res=>{
+                cy.get($res).should('contain','משה קי⁠בל תורה מסיני')
+            })
+        })
+    
+        
     })
 
-    
 })
+
