f0c8ce4cc3b29de1ba2f6828ea2ed290c6308bb9
ShlomoBluth <lomibluth@gmail.com>
2023-10-08 20:33:22 +0300

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  video: true,
   projectId: '2jrsy1',
   screenshotOnRunFailure: false,
   defaultCommandTimeout: 10000,
